050dc611578abff0a195b3517eb3571471f16aca
pk <purvikansara@gmail.com>
2024-01-17 22:51:09 -0800

    class app tested

---
+++ Lecture-2/.ipynb_checkpoints/SOLUTIONS_main_page-checkpoint.py
@@ -0,0 +1,10 @@
+import streamlit as st
+
+st.set_page_config()
+
+st.image('Images/money_tree.jpg')
+st.title('Choose page on sidebar:')
+st.subheader('Examine the training data')
+st.subheader('or')
+st.subheader('Either use and explore the model')
+
