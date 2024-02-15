431e76b48d6e960c7c73da942d3b1e57f4203cab
pk <purvikansara@gmail.com>
2024-02-14 18:32:42 -0800

    class work complete

---
+++ Lecture-2/pages/testing.py
@@ -0,0 +1,7 @@
+## Page Title
+import pandas as pd
+import streamlit as st
+import streamlit.components.v1 as components
+
+st.title('Testing Model Evaluation and Predictions')
+st.subheader(' Testing Evaluate the model or make a prediction to the left.')
