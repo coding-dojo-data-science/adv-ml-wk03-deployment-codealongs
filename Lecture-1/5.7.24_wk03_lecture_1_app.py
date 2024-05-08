bbcaf4a012b628453992ed94b2ffb8787712a142
Brenda Hungerford <bhungerford@codingdojo.com>
2024-05-07 21:36:09 -0400

    Update 5.7.24_wk03_lecture_1_app.py

--- Lecture-1/5.7.24_wk03_lecture_1_app.py
+++ Lecture-1/5.7.24_wk03_lecture_1_app.py
@@ -17,59 +17,84 @@ import functions as fn
 
 
 ## Define load_data function with caching
-
+@st.cache_data
+def load_data():
+    df = pd.read_csv('../Data/loan_approval.csv')
+    return df
 
 ## Use function to load Data
-
+df = load_data()
 
 
 ## Image, title and Markdown subheader
-
+st.image('../Images/money_tree.jpg')
+st.title('Loan Approval')
+st.markdown('Data gathered from [Kaggle](https://www.kaggle.com)')
 
 ## Display DataFrame
-
+st.dataframe(df)
 
 
 ## .info()
 ## Get string for .info()
-
+buffer = StringIO()
+df.info(buf=buffer)
+info_text = buffer.getvalue()
 
 #Display .info() with button trigger
-
+st.sidebar.subheader('Show Dataframe Summary')
+summary_text = st.sidebar.button('Summary Text')
+if summary_text:
+    st.text(info_text)
 
 ## Descriptive Statistics subheader
-
+st.sidebar.subheader('Show Descriptive Statistics')
 
 ## Button for Statistics
-
+show_stats = st.sidebar.button('Descriptive Statistics')
+if show_stats:
+    describe = df.describe()
+    st.dataframe(describe)
 
 ## Eda Plots subheader
-
+st.sidebar.subheader('Explore a Column')
 
 ## Columns for EDA
-
-
+columns = df.columns
+target = 'loan_status'
+features = [col for col in columns if col != target]
 
 ## selectbox for columns
-
+eda_column = st.sidebar.selectbox('Columns to explroe', columns, index = None)
 
 ## Conditional: if column was chosen
-
+if eda_column:
     ## Check if column is object or numeric and use appropriate plot function
-
+    if df[eda_column].dtype == 'object':
+        fig = fn.explore_categorical(df, eda_column)
+    else:
+        fig = fn.explore_numeric(df, eda_column)
     ## Show plot
+    st.subheader(f'Display Descriptive Plots for {eda_column}')
+    st.pyplot(fig)
 
 
 ## Select box for features vs target
-
+feature = st.sidebar.selectbox('Compare Feature to Target', features, index=None)
 
 ## Conditional: if feature was chosen
-
+if feature:
     ## Check if feature is numeric or object
-
+    if df[feature].dtype == 'object':
+        comparison = df.groupby('loan_status').count()
+        title = f'Count of {feature} by {target}'
+    else:
+        comparison = df.groupby('loan_status').mean()
+        title = f'Mean of {feature} by {target}'
 
     ## Display appropriate comparison
-
+    pfig = px.bar(comparison, y = feature, title=title)
+    st.plotly_chart(pfig)
         
     
 
